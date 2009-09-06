cbdfc0554f7d39b110a14a67728bf34511cfe5f3
ggiunta <ggiunta@013ecfd8-0664-425d-a759-9c98391dc3f9>
2009-09-06 09:42:25 +0000

    - added two files using during packaging of the distro
    git-svn-id: https://svn.code.sf.net/p/phpxmlrpc/code/trunk/javascript@69 013ecfd8-0664-425d-a759-9c98391dc3f9

---
+++ minify.php
@@ -0,0 +1,13 @@
+<?php
+
+define('JSMIN_AS_LIB', true); // prevents auto-run on include
+include('jsmin.php');
+// create the JSMin class, indicating no output file is desired
+array_shift($argv);
+foreach( $argv as $file )
+{
+    $jsMin = new JSMin($file, '-');
+    $jsMin->minify();
+}
+
+?>
