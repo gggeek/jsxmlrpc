a3316bb87ecc44b732cfdc4bfc6eeb87db19d6f0
gggeek <giunta.gaetano@gmail.com>
2022-12-15 17:02:45 +0000

    dynamically load required modules when on npm

--- demo/demo_node.js
+++ demo/demo_node.js
@@ -1,13 +1,5 @@
 import {xmlrpc_client, xmlrpcmsg} from '@jsxmlrpc/jsxmlrpc';
 
-// 'hack' 1 - this is required to insure the XMLHttpRequest object used by the library is available when on Node
-import XMLHttpRequest from 'xhr2';
-global.XMLHttpRequest = XMLHttpRequest;
-// 'hack' 2 - same with an xml parser
-import jsdom  from 'jsdom';
-const { JSDOM } = jsdom;
-global.DOMParser = new JSDOM().window.DOMParser;
-
 let client = new xmlrpc_client('https://gggeek.altervista.org/sw/xmlrpc/demo/server/server.php');
 let msg = new xmlrpcmsg('system.listMethods', []);
 client.setDebug(2);
@@ -15,5 +7,5 @@ client.setDebug(2);
 client.send(msg, 0, function(resp) {
     console.log('FAULT CODE: ' + resp.faultCode());
     console.log('FAULT STRING: ' +  resp.faultString());
-    console.log('VALUE: ' +  resp.value());
+    console.log('VALUE: ' +  resp.value().arrayMem(0).scalarVal());
 });
