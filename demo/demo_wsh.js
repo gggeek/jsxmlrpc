0c5cc34dba261bc90b386044cc739fb0d61c9673
gggeek <giunta.gaetano@gmail.com>
2022-12-15 18:14:19 +0000

    add notices to wsh demos

--- demo/demo_wsh.js
+++ demo/demo_wsh.js
@@ -1,12 +1,13 @@
 /**
  * Example script to take advantage of jsxmlrpc in a WSH script host environment
+ * ex: Cscript.exe demo_wsh.js
  *
  * @author Gaetano Giunta
+ *
+ * @todo this is currently broken. We need to transpile the library to ES5 for it to work...
  */
 
-// Import jsxmlrpc lib
-// trick from http://msdn.microsoft.com/msdnmag/issues/1100/cutting/
-function Include(jsFile) {
+function include(jsFile) {
     var fso = new ActiveXObject("Scripting.FileSystemObject");
     var f = fso.OpenTextFile(jsFile);
     var s = f.ReadAll();
@@ -14,7 +15,13 @@ function Include(jsFile) {
     return s;
 }
 
-eval(Include('xmlrpc_lib.js'));
+// Import jsxmlrpc lib - do a poor man's transpilation on the fly
+var source = include('..\\lib\\xmlrpc_lib.js');
+// strip ES6 features
+source = source.replace(/^export /mg, '');
+source = source.replace(/ polyfills [\s\S]+ global variables/, '');
+/// @todo other features we need to fix: usage of default function arguments, possibly get/set
+eval(source);
 
 var client = new xmlrpc_client('https://gggeek.altervista.org/sw/xmlrpc/demo/server/server.php');
 var msg = new xmlrpcmsg('system.listMethods', []);
