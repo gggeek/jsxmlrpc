07473327ea700d2ab428b5109edf2719f288db04
gggeek <giunta.gaetano@gmail.com>
2022-12-17 17:52:14 +0000

    rebuild doc-generation toolchain

--- doc/build/generate.sh
+++ doc/build/generate.sh
@@ -7,11 +7,20 @@ cd "$(dirname -- "$(dirname -- "$(dirname -- "${BASH_SOURCE[0]}")")")"
 # API docs from JS
 ./node_modules/.bin/jsdoc -R ./README.md -d doc/api lib/
 
-# Manual in html and pdf formats, from docbook source
-sed -r -i -e 's|%docbook-chunk.xsl%|./docbook-xsl/xhtml/chunk.xsl|g' doc/build/custom.xsl
-sed -r -i -e 's|%fo-docbook.xsl%|./docbook-xsl/fo/docbook.xsl|g' doc/build/custom.fo.xsl
-./node_modules/.bin/xslt3 -s:doc/build/xmlrpc_js.xml -xsl:doc/build/custom.xsl -o:doc/xmlrpc_js.html
-./node_modules/.bin/xslt3 -s:doc/build/xmlrpc_js.xml -xsl:doc/build/custom.fo.xsl -o:doc/build/xmlrpc_js.fo.xml
-fop doc/build/xmlrpc_js.fo.xml doc/xmlrpc_js.pdf
+# "Manual" in html and pdf formats, from docbook source
 
-rm doc/build/xmlrpc_js.fo.xml
+# Abandoned toolchain: xslt + fop
+#sed -r -i -e 's|%docbook-chunk.xsl%|./docbook-xsl/xhtml/chunk.xsl|g' doc/build/custom.xsl
+#sed -r -i -e 's|%fo-docbook.xsl%|./docbook-xsl/fo/docbook.xsl|g' doc/build/custom.fo.xsl
+## This fails because the docbook xslt files contain DTD entities, which are not supported by the Node xml parser.
+##./node_modules/.bin/xslt3 -s:doc/build/xmlrpc_js.xml -xsl:doc/build/custom.xsl -o:doc/xmlrpc_js.html
+##./node_modules/.bin/xslt3 -s:doc/build/xmlrpc_js.xml -xsl:doc/build/custom.fo.xsl -o:doc/build/xmlrpc_js.fo.xml
+## So we moved to xsltproc :-)
+#xsltproc -o doc/manual/ ./doc/build/custom.xsl doc/xmlrpc_js.xml
+#xsltproc -o doc/build/xmlrpc_js.fo.xml ./doc/build/custom.fo.xsl doc/xmlrpc_js.xml
+#fop doc/build/xmlrpc_js.fo.xml doc/xmlrpc_js.pdf
+#rm doc/build/xmlrpc_js.fo.xml
+
+# This toolchain works
+pandoc -s --from=docbook --to=html -o doc/xmlrpc_js.html xmlrpc_js.xml
+pandoc -s --from=docbook --to=pdf -o doc/xmlrpc_js.pdf doc/xmlrpc_js.xml
--- doc/build/setup_tools.sh
+++ doc/build/setup_tools.sh
@@ -4,17 +4,21 @@
 
 set -e
 
-DOCBOOK_XSL_VERSION=1.79.2
+# @todo install a more recent pandoc version straight from github, and use pagedjs-cli as pdf engine for it? texlive is huge...
 
 cd "$(dirname -- "${BASH_SOURCE[0]}")"
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
-    curl fop git unzip
+    curl git pandoc texlive unzip
+#    curl git fop unzip xsltproc
 
-# download and unzip the xslt package
-if [ ! -d docbook-xsl ]; then
-    curl -fsSL "https://github.com/docbook/xslt10-stylesheets/releases/download/release/${DOCBOOK_XSL_VERSION}/docbook-xsl-${DOCBOOK_XSL_VERSION}.zip" --output dbxsl.zip
-    unzip dbxsl.zip
-    mv "docbook-xsl-${DOCBOOK_XSL_VERSION}" docbook-xsl
-    rm dbxsl.zip
-fi
+# Download and unzip the dockbook xslt package
+# Commented out, since we now use pandoc, which has no built-in xslt support
+#DOCBOOK_XSL_VERSION="1.79.2"
+#if [ ! -d docbook-xsl ]; then
+#    # @todo what if dbxsl.zip exists?
+#    curl -fsSL "https://github.com/docbook/xslt10-stylesheets/releases/download/release/${DOCBOOK_XSL_VERSION}/docbook-xsl-${DOCBOOK_XSL_VERSION}.zip" --output dbxsl.zip
+#    unzip dbxsl.zip
+#    mv "docbook-xsl-${DOCBOOK_XSL_VERSION}" docbook-xsl
+#    rm dbxsl.zip
+#fi
